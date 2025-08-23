f0083ac9a45fc88365f8052cd72b726a7aa903f7
Michal-Om <michal_omri@yahoo.com>
2025-08-23 16:04:51 +0300

    Setup app structure with routing, redux provider, base pages/components, and event bus service

---
+++ src/cmps/AppHeader.jsx
@@ -0,0 +1,51 @@
+
+import { UserMsg } from './UserMsg.jsx'
+import { LoginSignup } from './LoginSignup.jsx'
+import { userService } from '../services/user.service.js'
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service.js'
+import { logout } from '../store/actions/user.actions.js'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+
+
+export function AppHeader() {
+    const dispatch = useDispatch()
+    const user = useSelector(storeState => storeState.userModule.loggedInUser)
+    // console.log('user:', user)
+
+    function onLogout() {
+        logout()
+            .then(() => {
+                showSuccessMsg('logout successfully')
+            })
+            .catch((err) => {
+                showErrorMsg('OOPs try again')
+            })
+    }
+
+
+    return (
+        <header className="app-header full main-layout">
+            <section className="header-container">
+                <h1>React Toy App!</h1>
+                <nav className="app-nav">
+                    <NavLink to="/" >Home</NavLink>
+                    <NavLink to="/about" >About</NavLink>
+                    <NavLink to="/toy" >Toys</NavLink>
+                </nav>
+            </section>
+            {user ? (
+                < section >
+                    <span to={`/user/${user._id}`}>Hello {user.fullname} <span>${user.score.toLocaleString()}</span></span>
+                    <button onClick={onLogout}>Logout</button>
+                </ section >
+            ) : (
+                <section>
+                    <LoginSignup />
+                </section>
+            )}
+            <UserMsg />
+        </header>
+    )
+}
