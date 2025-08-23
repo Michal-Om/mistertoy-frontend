a4ceca2057d21a184f78d8d8d94d91c52b36b353
Michal-Om <michal_omri@yahoo.com>
2025-08-23 16:47:09 +0300

    Add toy and user reducers, setup Redux store

---
+++ src/store/reducers/user.reducer.js
@@ -0,0 +1,15 @@
+export const SET_USER = 'SET_USER'
+
+const initialState = {
+    loggedinUser: null,
+}
+
+export function userReducer(state = initialState, action = {}) {
+    switch (action.type) {
+        case SET_USER:
+            return { ...state, loggedinUser: action.loggedinUser }
+
+        default:
+            return state
+    }
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,31 +1,12 @@
+import { createStore, combineReducers } from "redux"
+import { toyReducer } from "./reducers/toy.reducer.js"
+import { userReducer } from "./reducers/user.reducer.js"
 
-import { toyService } from "../../services/toy.service.js"
+const rootReducer = combineReducers({
+    toyModule: toyReducer,
+    userModule: userReducer,
+})
 
-export const SET_TOYS = 'SET_TOYS'
-export const REMOVE_TOY = 'REMOVE_TOY'
-export const ADD_TOY = 'ADD_TOY'
-export const UPDATE_TOY = 'UPDATE_TOY'
 
-export const SET_FILTER_BY = 'SET_FILTER_BY'
-
-const initialState = {
-    toys: [],
-    filterBy: toyService.getDefaultFilter(),
-}
-
-export function toyReducer(state = initialState, action = {}) {
-    switch (action.type) {
-        case SET_TOYS:
-            return { ...state, toys: action.toys }
-        case REMOVE_TOY:
-            return { ...state, toys: state.toys.filter(toy => toy._id !== action.toyId) }
-        case ADD_TOY:
-            return { ...state, toys: [...state.toys, action.toy] }
-        case UPDATE_TOY:
-            return { ...state, toys: state.toys.map(toy => toy._id === action.toy._id ? action.toy : toy) }
-        case SET_FILTER_BY:
-            return { ...state, filterBy: { ...state.filterBy, ...action.filterBy } }
-        default:
-            return state
-    }
-}
+export const store = createStore(rootReducer)
+window.gStore = store
