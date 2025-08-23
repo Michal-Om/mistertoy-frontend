53d8e83a4a997c5b9dd30306bf94581296b5d978
Michal-Om <michal_omri@yahoo.com>
2025-08-23 18:40:33 +0300

    Update App.jsx imports to connect pages and components for routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { Provider } from "react-redux"
 
 import './assets/style/main.css'
+import { AppHeader } from './cmps/AppHeader.jsx'
+import { AppFooter } from './cmps/AppFooter.jsx'
+import { HomePage } from './pages/HomePage.jsx'
+import { AboutUs } from './pages/AboutUs.jsx'
+import { ToyIndex } from './pages/ToyIndex.jsx'
+import { ToyEdit } from './pages/ToyEdit.jsx'
 import { ToyDetails } from "./pages/ToyDetails.jsx"
+import { UserDetails } from './pages/UserDetails.jsx'
 import { store } from "./store/store.js"
 
 export function App() {
